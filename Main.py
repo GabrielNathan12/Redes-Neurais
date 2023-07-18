6d32b04bd9060bae0ea02c3e08bbcccf9b94832a
Gabriel Nathan Almeida Silva Gabriel <76185909+GabrielNathan12@users.noreply.github.com>
2023-07-17 23:35:05 -0300

    feat: add alguns prints a mais na tela

--- Main.py
+++ Main.py
@@ -10,22 +10,22 @@ matrizDePeso = [
     [0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0],
     [0, 3, 0, 0, 0, 0],
-    [0, -4, 1, 0, 0, 0],
-    [0, -1, -3, 0, 0, 0],
-    [0, 0, 0, 2, -10, 0]
+    [0,-4, 1, 0, 0, 0],
+    [0,-1,-3, 0, 0, 0],
+    [0, 0, 0, 2,-10,0]
 ];
 
 amostraDados = [
-    (-3, 0.73212),
-    (-2, 0.7339),
-    (-1, 0.7838),
-    (-0.5, 0.8903),
-    (0, 0.982),
-    (0.5, 0.8114),
-    (1.0, 0.5937),
-    (1.5, 0.5219),
-    (2.0, 0.5049),
-    (3.0, 0.5002)
+        (-3.0, 0.73212),
+        (-2.0, 0.7339),
+        (-1.0, 0.7838),
+        (-0.5, 0.8903),
+        (0.0,  0.982),
+        (0.5,  0.8114),
+        (1.0,  0.5937),
+        (1.5,  0.5219),
+        (2.0,  0.5049),
+        (3.0,  0.5002)
 ];
 
 rede = RedeNeural(matrizDePeso);
@@ -34,9 +34,10 @@ delta = [rede.delta1, rede.delta2, rede.delta3, rede.delta4];
 
 treine = TreinamentoDaRede(matrizDePeso, y, delta, amostraDados);
 
+print("Valores dos dados de teste x=0.0 e y = 0.0");
 # Imprime os dados de teste para x=0.0 e yt=0.5
 treine.imprimirDados(0.0, matrizDePeso, 0.5);
-
+print("Valores dos dados de teste x=1.0 e y = 0.1");
 # Imprime os dados de teste para x=1.0 e yt=0.1
 treine.imprimirDados(1.0, matrizDePeso, 0.1);
 
